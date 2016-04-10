6c389388c5677cdb64022e4ee7fbaf71aeb8ea92
firm1 <dralliw350@gmail.com>
2016-04-10 02:51:56 +0200

    make dynamic versionning on about page

--- src/main/java/com/zestedesavoir/zestwriter/view/dialogs/AboutDialog.java
+++ src/main/java/com/zestedesavoir/zestwriter/view/dialogs/AboutDialog.java
@@ -1,11 +1,41 @@
 package com.zestedesavoir.zestwriter.view.dialogs;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.zestedesavoir.zestwriter.MainApp;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class AboutDialog{
     private MainApp mainApp;
+    private Logger logger;
+    @FXML private Label version;
+
+
+    public AboutDialog() {
+        logger = LoggerFactory.getLogger(AboutDialog.class);
+    }
+
+    @FXML private void initialize() {
+        Properties props = new Properties();
+        try {
+            InputStream input = new FileInputStream("gradle.properties");
+            props.load(input);
+            version.setText(props.getProperty("version", "Inconnue"));
+            input.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+    }
 
     public void setMainApp(MainApp mainApp){
         this.mainApp = mainApp;
