2a684405a23a9bf4623ebbd9880a628d608f60e3
SpaceFox <ujueseo.yeou@gmail.com>
2016-05-10 00:01:10 +0200

    No concatenation on append() chains

--- src/main/java/com/zestedesavoir/zestwriter/MainApp.java
+++ src/main/java/com/zestedesavoir/zestwriter/MainApp.java
@@ -222,7 +222,7 @@ public class MainApp extends Application {
         scene.addEventFilter(KeyEvent.KEY_PRESSED, t -> {
             String codeStr = t.getCode().toString();
             if(!key.toString().endsWith("_"+codeStr)){
-                 key.append("_"+codeStr);
+                 key.append("_").append(codeStr);
             }
         });
         scene.addEventFilter(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
