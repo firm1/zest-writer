4a9fba1c6339cc388f7d3bd23ca0568e826fbddf
hcourte <hugo.courtecuisse@gmail.com>
2016-07-01 14:54:08 +0200

    Add new container and extract test

---
+++ src/test/java/integration/AddNewContainerAndExtractTest.java
@@ -0,0 +1,58 @@
+package integration;
+
+import annotation.MediumTest;
+import com.zestedesavoir.zestwriter.MainApp;
+import integration.util.UtilFixtureLeGuideDuContributeur;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.api.FxRobot;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
+
+import static org.testfx.api.FxToolkit.registerPrimaryStage;
+import static org.testfx.api.FxToolkit.setupApplication;
+import static org.testfx.api.FxToolkit.setupStage;
+
+public class AddNewContainerAndExtractTest extends FxRobot {
+
+    private MainApp mainApp;
+
+    @BeforeClass
+    public static void setupSpec() throws Exception {
+        Stage primaryStage = registerPrimaryStage();
+        setupStage(stage -> stage.show());
+    }
+
+    @Before
+    public void setup() throws Exception {
+      mainApp = (MainApp) setupApplication(MainApp.class);
+    }
+
+    @Test
+    @MediumTest
+    public void createNewContainerAndExtract() throws IOException, InterruptedException, URISyntaxException {
+        UtilFixtureLeGuideDuContributeur.loadFixtureLeGuideDuContributeur(mainApp);
+
+        sleep(2, TimeUnit.SECONDS);
+        clickOn("Contribuer au contenu", MouseButton.SECONDARY);
+        clickOn("Ajouter un conteneur");
+        clickOn("OK");
+
+        doubleClickOn("Contribuer au contenu");
+        clickOn("Conteneur", MouseButton.SECONDARY);
+
+        clickOn("Ajouter un extrait");
+        clickOn("OK");
+
+        doubleClickOn("Contribuer au contenu");
+        doubleClickOn("Conteneur");
+        doubleClickOn("Extrait");
+    }
+
+
+}
