e261ddbb4d9b06af5d46dfd23a3adf3e08b3285d
hcourte <hugo.courtecuisse@gmail.com>
2016-07-02 11:38:41 +0200

    Add method getPathFixtureLeGuideDuContributeur

--- src/test/java/integration/util/Configuration.java
+++ src/test/java/integration/util/Configuration.java
@@ -3,6 +3,9 @@ package integration.util;
 import com.zestedesavoir.zestwriter.MainApp;
 
 import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Configuration {
 
@@ -11,6 +14,13 @@ public class Configuration {
 
     public static String getWorkspacePath (MainApp mainApp) {
         return com.zestedesavoir.zestwriter.utils.Configuration.getDefaultWorkspace() + File.separator + "offline" ;
+    }
+
+    public static String getPathFixtureLeGuideDuContributeur(){
+        URL location = UtilFixtureLeGuideDuContributeur.class.getResource("/fixtures/le-guide-du-contributeur/manifest.json");
+        String pathWithFileName = location.getPath().toString().replaceFirst("^/(.:/)", "$1");
+
+        return Paths.get(pathWithFileName).getParent().toString();
     }
 
 }
