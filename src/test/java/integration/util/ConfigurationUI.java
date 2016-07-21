f97e8caff52a42714932a85b3d8b93a4263bd538
Hugo Courtecuisse <hugo.courtecuisse@gmail.com>
2016-07-21 13:51:15 +0200

    Start refacto to pass every label in a separate file.

---
+++ src/test/java/integration/util/ConfigurationUI.java
@@ -0,0 +1,28 @@
+package integration.util;
+
+public class ConfigurationUI {
+
+    public static String SAMPLE_TEXT_HELLO_WORLD = "Hello World";
+    public static String SAMPLE_URL = "https://zestedesavoir.com/";
+
+    public static String ID_EDITOR = "#editor";
+    public static String ID_EDITOR_BOLD = "#bold";
+    public static String ID_EDITOR_ITALIC = "#italic";
+    public static String ID_EDITOR_STRIKETHROUGH = "#strikethrough";
+    public static String ID_EDITOR_KEYBOARD = "#keyboard";
+    public static String ID_EDITOR_SUPERSCRIPT = "#superscript";
+    public static String ID_EDITOR_SUBSCRIPT = "#subscript";
+    public static String ID_EDITOR_CENTER = "#center";
+    public static String ID_EDITOR_RIGHT = "#right";
+    public static String ID_EDITOR_BULLET = "#bullet";
+    public static String ID_EDITOR_NUMBERED = "#numbered";
+    public static String ID_EDITOR_HEADER = "#header";
+    public static String ID_EDITOR_QUOTE = "#quote";
+    public static String ID_EDITOR_LINK = "#link";
+    public static String ID_EDITOR_SAVEBUTTON = "#SaveButton";
+
+    public static String LABEL_BUTTON_OK_LINK_WINDOW = "OK";
+
+    public static String CLASS_OVERFLOW_BUTTON_TITLE_BAR = ".tool-bar-overflow-button";
+
+}
