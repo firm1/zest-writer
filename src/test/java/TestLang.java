68190d99b8041841049d85f61cd9b6f27526af6c
firm1 <dralliw350@gmail.com>
2016-07-25 12:29:50 +0200

    add test for availables langs keys

---
+++ src/test/java/TestLang.java
@@ -0,0 +1,28 @@
+import com.zestedesavoir.zestwriter.utils.Lang;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+
+public class TestLang {
+    @Test
+    public void testKeyLang() {
+        List<Lang> langs = Lang.langAvailable;
+        List<Set<String>> keys = new ArrayList<>();
+        for(Lang lang:langs) {
+            ResourceBundle bundle = ResourceBundle.getBundle("locales/ui", lang.getLocale());
+            keys.add(bundle.keySet());
+        }
+        Set<String> firstLang = keys.get(0);
+        for(Set<String> key:keys) {
+            assertEquals(key.size(), firstLang.size());
+            assertArrayEquals(key.toArray(), firstLang.toArray());
+        }
+    }
+}
