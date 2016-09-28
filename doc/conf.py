335fa53eff59f1af8abcca769f05cc3ae5065f92
Nathanael Jourdane <nathanael.jourdane@irap.omp.eu>
2016-09-28 10:57:36 +0200

    rename readme.rst to index.rst to make rtd happy.

--- doc/conf.py
+++ doc/conf.py
@@ -97,7 +97,7 @@ source_suffix = ['.rst']
 source_encoding = 'utf-8'
 
 # The master toctree document.
-master_doc = 'readme'
+master_doc = 'index'
 
 # General information about the project.
 project = u'Zest Writer'
