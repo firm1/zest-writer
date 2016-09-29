1a1cf34425f70bf9067ebab57c74db41c44d67fc
Nathanaël Jourdane <roipoussiere@gmail.com>
2016-09-29 09:11:21 +0200

    Remove code comments in class Textual

--- src/main/java/com/zestedesavoir/zestwriter/model/Textual.java
+++ src/main/java/com/zestedesavoir/zestwriter/model/Textual.java
@@ -4,41 +4,7 @@ public interface Textual {
 
     String getMarkdown();
     void save();
-//    {
-//        BufferedWriter writer = null;
-//        try {
-//            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getFilePath()), "UTF8"));
-//            writer.append(getMarkdown());
-//            writer.flush();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (writer != null) {
-//                    writer.close();
-//                }
-//            } catch (Exception ignored) {
-//            }
-//        }
-//    }
     String readMarkdown();
-//    {
-//        Path path = Paths.get(this.getFilePath());
-//        Scanner scanner;
-//        StringBuilder bfString = new StringBuilder();
-//        try {
-//            scanner = new Scanner(path, StandardCharsets.UTF_8.name());
-//            while (scanner.hasNextLine()) {
-//                bfString.append(scanner.nextLine());
-//                bfString.append("\n");
-//            }
-//            scanner.close();
-//            return bfString.toString();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return "";
-//    }
     void loadMarkdown(); // { setMarkdown(readMarkdown()); }
     String getTitle();
     void setMarkdown(String markdown);
