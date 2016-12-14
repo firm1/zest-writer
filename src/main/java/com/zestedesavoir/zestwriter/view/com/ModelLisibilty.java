d814d4bc3c72dc906817e7d9812ae8ba8a70a09d
firm1 <contact@firm1.eu>
2016-12-14 11:46:06 +0100

    better lisibility of lisibility table

---
+++ src/main/java/com/zestedesavoir/zestwriter/view/com/ModelLisibilty.java
@@ -0,0 +1,23 @@
+package com.zestedesavoir.zestwriter.view.com;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import org.controlsfx.control.Rating;
+
+public class ModelLisibilty {
+    private final SimpleStringProperty text;
+    private final SimpleObjectProperty rating;
+
+    public ModelLisibilty(String text, Rating rating) {
+        this.text = new SimpleStringProperty(text);
+        this.rating = new SimpleObjectProperty(rating);
+    }
+
+    public String getText() {
+        return text.get();
+    }
+
+    public Object getRating() {
+        return rating.get();
+    }
+}
