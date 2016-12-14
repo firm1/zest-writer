2c8a4e98fe5b814f0b4bdf1143c1ccbcdbbae7d6
firm1 <contact@firm1.eu>
2016-12-14 18:54:27 +0100

    update sonar

---
+++ scripts/travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+    echo 'Internal pull request: trigger QA and analysis'
+
+    ../gradlew sonarqube \
+        -Dzw.username=$ZDS_USERNAME \
+        -Dzw.password=$ZDS_PASSWORD \
+        -Dzw.github_user=$GITHUB_USER \
+        -Dzw.github_token=$GITHUB_TOKEN \
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.analysis.mode=preview \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN
+else
+    ../gradlew check --info \
+            -Dzw.username=$ZDS_USERNAME \
+            -Dzw.password=$ZDS_PASSWORD \
+            -Dzw.github_user=$GITHUB_USER \
+            -Dzw.github_token=$GITHUB_TOKEN \
+            -Dsonar.login=$SONAR_TOKEN
+fi
