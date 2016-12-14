d149ddfc5d3097e9957b0f0d1b0c84480d3ad6fc
firm1 <contact@firm1.eu>
2016-12-14 23:22:21 +0100

    fix sonar

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,12 +12,14 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
         -Dsonar.analysis.mode=preview \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_TOKEN
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=https://sonar.winxaito.com
 else
     ./gradlew sonarqube --info \
             -Dzw.username=$ZDS_USERNAME \
             -Dzw.password=$ZDS_PASSWORD \
             -Dzw.github_user=$GITHUB_USER \
             -Dzw.github_token=$GITHUB_TOKEN \
-            -Dsonar.login=$SONAR_TOKEN
+            -Dsonar.login=$SONAR_TOKEN \
+            -Dsonar.host.url=https://sonar.winxaito.com
 fi
