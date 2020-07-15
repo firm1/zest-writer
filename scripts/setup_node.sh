3702ab046f3ab310cd5e7243a5132e51f69e396f
firm1 <contact@firm1.eu>
2020-07-15 18:33:54 +0200

    update npm setup

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
-source ~/.profile
-
+curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh --output /tmp/install.sh
+source /tmp/install.sh
+source ~/.nvm/nvm.sh
 nvm ls-remote
-
 nvm install "$NODE_VERSION"
 nvm use "$NODE_VERSION"
-
 node -v
